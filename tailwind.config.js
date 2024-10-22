ba43626400fda7829834fde4f8047f85ca0f0141
abimanyupewe <abimanyupw369@gmail.com>
2024-10-22 20:22:24 +0700

    baru

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,42 @@
 module.exports = {
   content: ['home.html'],
   theme: {
-    extend: {},
+    extend: {
+      // ini untuk menambahkan fitur yang sudah dimiliki tailwind
+      colors: {
+        // beige_gw: "#FEF3E2",
+        // beige2_gw: "#FFEAC5",
+        // green_gw: "#0A6847",
+        // green_pastel: "#A5DD9B",
+      },
+
+      // font
+      fontFamily: {
+        // poppins: ["Poppins", "sans-serif"],
+      },
+
+      // ukuran web
+      screens: {
+        "2xl": "1320px",
+      },
+
+      // animations
+      animation: {
+        // "scale-in-out": "scaleInOut 100s ease-in-out infinite",
+        // "rotate-slow": "rotateSlow 100s linear infinite",
+      },
+
+      keyframes: {
+        // scaleInOut: {
+        //   "0%, 100%": { transform: "scale(1.1)" },
+        //   "50%": { transform: "scale(1)" },
+        // },
+        // rotateSlow: {
+        //   "0%": { transform: "rotate(0deg)" },
+        //   "100%": { transform: "rotate(360deg)" },
+        // },
+      },
+    },
   },
   plugins: [],
 }
